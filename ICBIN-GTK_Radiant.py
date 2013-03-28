818a7952ebf78b9f795ec92f2ccf1b19a974e2e0
kairosfractes <kairosfractes@gmail.com>
2013-03-28 02:22:19 -0400

    Uncle Jemima's Alpha
    DEFINITELY NOT RADIANT

---
+++ ICBIN-GTK_Radiant.py
@@ -0,0 +1,33 @@
+#some basic shapes and architectural stuff below this
+
+#def hollow_cube(length):
+#    newmap.write(
+
+#initialize variables
+textures = ["gothic_block/blocks18c_3"]
+entitynum = 0
+brushnum = 0
+
+#initialize map file, write first lines
+newmap = open("generation.map", "w")
+newmap.write('// entity '+str(entitynum)+' \n{ \n"classname" "worldspawn" \n')
+
+
+
+
+
+
+
+
+
+
+
+#testing material
+"""
+newmap.write('\n( 128 0 0 ) ( 128 1 0 ) ( 128 0 1 ) '+textures[0]+' 0 0 0 0.5 0.5 0 0 0')
+newmap.write('\n( 256 0 0 ) ( 256 0 1 ) ( 256 1 0 ) '+textures[0]+' 0 0 0 0.5 0.5 0 0 0')
+newmap.write('\n( 0 128 0 ) ( 0 128 1 ) ( 1 128 0 ) '+textures[0]+' 0 0 0 0.5 0.5 0 0 0')
+newmap.write('\n( 0 384 0 ) ( 1 384 0 ) ( 0 384 1 ) '+textures[0]+' 0 0 0 0.5 0.5 0 0 0')
+newmap.write('\n( 0 0 64 ) ( 1 0 64 ) ( 0 1 64 ) '+textures[0]+' 0 0 0 0.5 0.5 0 0 0')
+newmap.write('\n( 0 0 128 ) ( 0 1 128 ) ( 1 0 128 ) '+textures[0]+' 0 0 0 0.5 0.5 0 0 0\n}')
+"""
